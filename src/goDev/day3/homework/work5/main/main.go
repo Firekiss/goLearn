7d53216ab84cc96eb9d91c2d1812e9f00e654b57
zhangzhitao1992 <1084024036@qq.com>
2019-11-27 15:28:18 +0800

    添加大数相加作业diamante

---
+++ src/goDev/day3/homework/work5/main/main.go
@@ -0,0 +1,76 @@
+// 两个大叔相加求和
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func bigIntAdd(a string, b string) string {
+	var (
+		lenA   = len(a)
+		lenB   = len(b)
+		maxLen int
+		minLen int
+		max    string
+		min    string
+		sum    string
+		isOver bool
+	)
+
+	const ZERO = 48
+
+	if lenA > lenB {
+		max = a
+		min = b
+		maxLen = lenA
+		minLen = lenB
+	} else {
+		max = b
+		min = a
+		maxLen = lenB
+		minLen = lenA
+	}
+
+	for i, _ := range max {
+		// 没有达到小数的边界
+		if i < minLen {
+			maxV := int(max[maxLen-i-1]) - ZERO
+			minV := int(min[minLen-i-1]) - ZERO
+			singleSum := maxV + minV
+
+			if isOver {
+				singleSum++
+				isOver = false
+			}
+
+			if singleSum > 10 {
+				isOver = true
+				singleSum -= 10
+			}
+
+			sum = strconv.Itoa(singleSum) + sum
+		} else {
+			// 达到小数边界
+			singleVal := int(max[maxLen-i-1]) - ZERO
+
+			if isOver {
+				singleVal++
+				isOver = false
+			}
+
+			if singleVal > 10 {
+				isOver = true
+				singleVal -= 10
+			}
+
+			sum = strconv.Itoa(singleVal) + sum
+		}
+	}
+
+	return sum
+}
+
+func main() {
+	fmt.Printf("2464654876879876746464698 + 2323215454445 的结果是: %s\n", bigIntAdd("2464654876879876746464698", "2323215454445"))
+}
