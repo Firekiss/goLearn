82b03158d73571187b70e66c43ecb0ade17fb529
zhangzhitao1992 <1084024036@qq.com>
2019-11-28 18:01:44 +0800

    添加数组和切片学习代码

---
+++ src/goDev/day4/example1/main/main.go
@@ -0,0 +1,22 @@
+// 非递归编写斐波那契额数列前100个值
+// https://studygolang.com/articles/9425
+package main
+
+import "fmt"
+
+func printFebs(num int) []uint64 {
+	febs := make([]uint64, num)
+
+	febs[0] = 1
+	febs[1] = 1
+
+	for i := 2; i < len(febs); i++ {
+		febs[i] = febs[i-1] + febs[i-2]
+	}
+
+	return febs
+}
+
+func main() {
+	fmt.Printf("前1000个斐波那契数列是 %v\n", printFebs(1000))
+}
